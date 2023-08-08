85c2b003aba925cb6e883541dd9ed1d538f7b8be
NamanMevada <141706482+NamanMevada@users.noreply.github.com>
2023-08-08 18:28:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const registrationForm = document.getElementById('registrationForm');
-const registeredData = document.getElementById('registeredData');
+const entriesBody = document.getElementById('entriesBody');
 
-registrationForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+registrationForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    const name = registrationForm.name.value;
+    const email = registrationForm.email.value;
+    const dob = registrationForm.dob.value;
+    
+    // Validate age (between 18 and 55 years old)
+    const birthDate = new Date(dob);
+    const currentDate = new Date();
+    const age = currentDate.getFullYear() - birthDate.getFullYear();
+    if (age < 18 || age > 55) {
+        alert('Sorry, only users between 18 and 55 years old are allowed.');
+        return;
+    }
 
-  const name = registrationForm.name.value;
-  const email = registrationForm.email.value;
-
-  const newRow = document.createElement('tr');
-  newRow.innerHTML = `
-    <td>${name}</td>
-    <td>${email}</td>
-  `;
-
-  registeredData.appendChild(newRow);
-
-  registrationForm.reset();
+    // Add entry to the table
+    const newRow = entriesBody.insertRow();
+    newRow.innerHTML = `<td>${name}</td><td>${email}</td><td>${dob}</td>`;
+    
+    // Clear the form
+    registrationForm.reset();
 });
